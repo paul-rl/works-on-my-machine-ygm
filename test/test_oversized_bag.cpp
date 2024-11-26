b36bae1f11e1bf8867867160a7c821a0b9615f92
paul-rl <prl7.dev@gmail.com>
2024-11-25 22:19:41 -0500

    Isolated rank0 async insert test

--- test/test_oversized_bag.cpp
+++ test/test_oversized_bag.cpp
@@ -34,92 +34,13 @@ int main(int argc, char** argv) {
       bbag.async_insert("apple");
       bbag.async_insert("red");
     }
+    world.barrier();
     // Count and size functions come from overarching ygm container,
     // rely on local functions
+    std::cout << "Dog count: " << bbag.count("dog") << std::endl;
     YGM_ASSERT_RELEASE(bbag.count("dog") == 1);
     YGM_ASSERT_RELEASE(bbag.count("apple") == 1);
     YGM_ASSERT_RELEASE(bbag.count("red") == 1);
     YGM_ASSERT_RELEASE(bbag.size() == 3);
   }
-
-  // 
-
-  // Currently, shuffle functions are being ignored as per Roger
-  // Test local_shuffle and global_shuffle
-  // {
-  //   ygm::container::oversized_bag<int> bbag(world);
-  // 
-  // }
-
-  // 
-  // Test for_all
-  {
-    ygm::container::oversized_bag<std::string> bbag(world);
-    if (world.rank0()) {
-      bbag.async_insert("dog");
-      bbag.async_insert("apple");
-      bbag.async_insert("red");
-    }
-    int count{0};
-    bbag.for_all([&count](std::string& mstr) { ++count; });
-    int global_count = world.all_reduce_sum(count);
-    world.barrier();
-    YGM_ASSERT_RELEASE(global_count == 3);
-  }
-
-  //
-  // Test for_all (pair)
-  {
-    ygm::container::oversized_bag<std::pair<std::string, int>> pbag(world);
-    if (world.rank0()) {
-      pbag.async_insert({"dog", 1});
-      pbag.async_insert({"apple", 2});
-      pbag.async_insert({"red", 3});
-    }
-    int count{0};
-    pbag.for_all(
-      [&count] (std::pair<std::string, int>& mstr) { count += mstr.second; });
-    int global_count = world.all_reduce_sum(count);
-    world.barrier();
-    YGM_ASSERT_RELEASE(global_count == 6);
-  }
-
-  //
-  // Test for_all (split pair)
-  // {
-  //   ygm::container::oversized_bag<std::pair<std::string, int>> pbag(world);
-  //   if (world.rank0()) {
-  //     pbag.async_insert({"dog"}, 1);
-  //     pbag.async_insert({"apple"}, 2);
-  //     pbag.async_insert({"red"}, 3);
-  //   }
-  //   int count{0};
-  //   pbag.for_all(
-  //     [&count](std::string& first, int& second) { count += second; });
-  //   int global_count = world.all_reduce_sum(count);
-  //   world.barrier();
-  //   YGM_ASSERT_RELEASE(global_count == 6);
-  // }
-
-  //
-  // Test rebalance
-  {
-    ygm::container::oversized_bag<std::string> bbag(world);
-
-  }
-
-  //
-  // Test rebalance with non-standard rebalance sizes
-  {
-    ygm::container::oversized_bag<std::string> bbag(world);
-
-  }
-
-  //
-  // Test output data after rebalance
-  {
-    ygm::container::oversized_bag<int> bbag(world);
-
-  }
-
 }
